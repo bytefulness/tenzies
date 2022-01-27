a49ff6dbe96c4c92517295ed13a42ff43c692bef
eakyel <e.akyel@outlook.com>
2022-01-27 21:18:47 +0300

    Add high score feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Die from './Die';
 import { nanoid } from 'nanoid';
 import Confetti from 'react-confetti';
 
 export default function App() {
-  const [dice, setDice] = React.useState(allNewDice());
-  const [tenzies, setTenzies] = React.useState(false);
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+  const [highScore, setHighScore] = useState(0);
+  const [rollCount, setRollCount] = useState(0);
 
   React.useEffect(() => {
     const allHeld = dice.every((die) => die.isHeld);
@@ -13,7 +15,15 @@ export default function App() {
     const allSameValue = dice.every((die) => die.value === firstValue);
     if (allHeld && allSameValue) {
       setTenzies(true);
+      setHighScore((prevHighScore) => {
+        if (prevHighScore === 0) return rollCount;
+        if (prevHighScore > rollCount) return rollCount;
+        if (prevHighScore < rollCount) return prevHighScore;
+      });
+
+      setRollCount(0);
     }
+    console.log(highScore);
   }, [dice]);
 
   function generateNewDie() {
@@ -43,6 +53,8 @@ export default function App() {
       setTenzies(false);
       setDice(allNewDice());
     }
+
+    setRollCount((prevRollCount) => prevRollCount + 1);
   }
 
   function holdDice(id) {
@@ -70,6 +82,12 @@ export default function App() {
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
+      <h3 className="high-score">
+        High Score: {highScore ? highScore + ' Roll' : '👀'}
+      </h3>
+      <h3 className="rolled">
+        {rollCount ? `You rolled ${rollCount} times` : ''} 🎲
+      </h3>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
         {tenzies ? 'New Game' : 'Roll'}
